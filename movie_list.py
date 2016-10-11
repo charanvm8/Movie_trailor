9376f3262c17439d98ca1e9e9728b1c2886169ff
charanvm8 <charanvm8@gmail.com>
2016-10-10 22:37:34 -0400

    modified movie_list

--- movie_list.py
+++ movie_list.py
@@ -1,7 +1,7 @@
 import movie
 import fresh_tomatoes
 
-
+#Creating movie objects
 race_gurram=movie.Movie("Race Gurram",
                         "https://i.ytimg.com/vi/db_2mujPXjk/movieposter.jpg",
                         "https://www.youtube.com/watch?v=yCt-YUbs7H4")
@@ -17,7 +17,7 @@ aarya_2=movie.Movie("Aarya 2",
                 "https://images-na.ssl-images-amazon.com/images/M/MV5BMjkwNWEyYTQtZTUyOS00MTg5LTk3YjQtOTY3ZjlhZmE5MjFmXkEyXkFqcGdeQXVyMzA0NTI2OTM@._V1_UY268_CR9,0,182,268_AL_.jpg",
                 "https://www.youtube.com/watch?v=vpfXFqlLA3A")
 
-
+#Storing movie objects in a list
 movies=[race_gurram,so_satyamurty,sarainodu,aarya_2]
-
+#Calling method from fresh tomatoes 
 fresh_tomatoes.open_movies_page(movies)
